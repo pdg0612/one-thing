39eed97017f9b4eb7c9aaa13bb479cfe4677ee9d
pdg0612 <pdg0612tow@gmail.com>
2024-03-04 14:55:58 +0900

    first commit

---
+++ src/main/java/kr/hs/gbsw/appdev/AppdevApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.gbsw.appdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppdevApplication.class, args);
+	}
+
+}
